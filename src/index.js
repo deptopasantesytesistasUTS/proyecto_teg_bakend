b15a20959f89272a3f1290d5fea2647d1b2f3ed5
deptopasantesytesistasUTS <orianadaran1@gmail.com>
2025-07-07 21:10:09 -0400

    mas arreglos + prisma

--- src/index.js
+++ src/index.js
@@ -2,13 +2,7 @@ import express from 'express';
 import { PORT } from './config.js';
 import morgan from 'morgan';
 import './db.js'; // Importar para ejecutar la conexión a PostgreSQL
-import { corsMiddleware } from './middlewares/cors.js';
-import pacientesRoutes from './routes/pacientes.js';
-import expedientesRoutes from './routes/expedientes.js';
-import doctoresRoutes from './routes/doctores.js';
-import horariosRoutes from './routes/horarios.js';
-import consultasRoutes from './routes/consultas.js';
-import usuariosRoutes from './routes/usuarios.js';
+
 
 const app = express();
 
@@ -17,13 +11,7 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(corsMiddleware);
 
-// Rutas
-app.use('/api/pacientes', pacientesRoutes);
-app.use('/api/expedientes', expedientesRoutes);
-app.use('/api/doctores', doctoresRoutes);
-app.use('/api/horarios', horariosRoutes);
-app.use('/api/consultas', consultasRoutes);
-app.use('/api/usuarios', usuariosRoutes);
+
 
 // Ruta de prueba
 app.get('/', (req, res) => {
